cdbcd802afb46e0d3b80e1746d0d514e3b14ba19
Laura Li <xll@sfu.ca>
2015-02-15 14:56:13 -0800

    Add portfolio files

---
+++ css/mainJS.js
@@ -0,0 +1,51 @@
+// for witchase
+function witchase1() {
+	document.getElementById("witchase").src = "img/witchase/1.png"
+}
+function witchase2() {
+	document.getElementById("witchase").src = "img/witchase/2.png"
+}
+function witchase3() {
+	document.getElementById("witchase").src = "img/witchase/3.jpeg"
+}
+function witchase4() {
+	document.getElementById("witchase").src = "img/witchase/4.png"
+}
+function witchase5() {
+	document.getElementById("witchase").src = "img/witchase/5.jpeg"
+}
+function witchase6() {
+	document.getElementById("witchase").src = "img/witchase/6.png"
+}
+function witchase7() {
+	document.getElementById("witchase").src = "img/witchase/7.png"
+}
+function witchase8() {
+	document.getElementById("witchase").src = "img/witchase/8.png"
+}
+function witchase9() {
+	document.getElementById("witchase").src = "img/witchase/9.png"
+}
+function witchase10() {
+	document.getElementById("witchase").src = "img/witchase/10.png"
+}
+
+// for little sheep
+function littleSheep1() {
+	document.getElementById("littleSheep").src = "img/littleSheep/1.png"
+}
+function littleSheep2() {
+	document.getElementById("littleSheep").src = "img/littleSheep/2.png"
+}
+function littleSheep3() {
+	document.getElementById("littleSheep").src = "img/littleSheep/3.png"
+}
+function littleSheep4() {
+	document.getElementById("littleSheep").src = "img/littleSheep/4.png"
+}
+function littleSheep5() {
+	document.getElementById("littleSheep").src = "img/littleSheep/5.png"
+}
+function littleSheep6() {
+	document.getElementById("littleSheep").src = "img/littleSheep/6.png"
+}
