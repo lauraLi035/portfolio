68463c5559a86a0a39b8ea21ee99a2855933c998
Laura Li <xll@sfu.ca>
2015-03-19 11:39:39 -0700

    update

--- appInterface/inlivoCtrl.js
+++ appInterface/inlivoCtrl.js
@@ -8,20 +8,74 @@ angular.module('portfolio').controller('inlivoCtrl', function ($scope) {
     img: "../img/inlivo/persona2.jpg"
   }];
 
-  //   var sketchesSlides = $scope.sketchesSlides = [
-  // {
-  //   img: "../img/witchase/sketches1.jpg",
-  //   text: "game logic sketches"
-  // },{
-  //   img: "../img/witchase/sketches2.jpg",
-  //   text: "game and inventory interface"
-  // },{
-  //   img: "../img/witchase/sketches3.jpg",
-  //   text: "sketch of storyboard and progression map"
-  // },{
-  //   img: "../img/witchase/sketches4.png",
-  //   text: "digital game logic sketches"
-  // }
-  // ];
+    var slides = $scope.slides = [
+  {
+    img: "../img/inlivo/1.jpg",
+    text: "start page"
+  },{
+    img: "../img/inlivo/2.jpg",
+    text: "start page"
+  },{
+    img: "../img/inlivo/3.jpg",
+    text: "start page"
+  },{
+    img: "../img/inlivo/4.jpg",
+    text: "start page"
+  },{
+    img: "../img/inlivo/5.jpg",
+    text: "start page"
+  },{
+    img: "../img/inlivo/6.jpg",
+    text: "start page"
+  },{
+    img: "../img/inlivo/7.jpg",
+    text: "start page"
+  },{
+    img: "../img/inlivo/8.jpg",
+    text: "start page"
+  },{
+    img: "../img/inlivo/9.jpg",
+    text: "start page"
+  },{
+    img: "../img/inlivo/10.jpg",
+    text: "start page"
+  },{
+    img: "../img/inlivo/11.jpg",
+    text: "start page"
+  },{
+    img: "../img/inlivo/12.jpg",
+    text: "start page"
+  },{
+    img: "../img/inlivo/13.jpg",
+    text: "start page"
+  },{
+    img: "../img/inlivo/14.jpg",
+    text: "start page"
+  },{
+    img: "../img/inlivo/15.jpg",
+    text: "start page"
+  },{
+    img: "../img/inlivo/16.jpg",
+    text: "start page"
+  },{
+    img: "../img/inlivo/17.jpg",
+    text: "start page"
+  },{
+    img: "../img/inlivo/18.jpg",
+    text: "start page"
+  },{
+    img: "../img/inlivo/19.jpg",
+    text: "start page"
+  },{
+    img: "../img/inlivo/20.jpg",
+    text: "start page"
+  },{
+    img: "../img/inlivo/21.jpg",
+    text: "start page"
+  },{
+    img: "../img/inlivo/22.jpg",
+    text: "start page"
+  }
+  ];
 
 });
